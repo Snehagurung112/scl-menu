4b8a7293f725c40b9bf91f65f642c4eb94dc05e5
Snehagurung112 <snehasnehagurung@gmail.com>
2024-12-08 16:45:08 +0545

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,37 @@
+
+import React from 'react';
+
+
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+    <h3> Our Address</h3>
+    <p>Pokhara-kaski, Rajakochautara-32</p>
+ </div>
+ <div className="footer-content">
+    <h3> Our Contact</h3>
+    <p> brahmarupahss13@gmail.com</p>
+    <p> +977-61-410055</p>
+    <p>+977-61-410057</p>
+ </div>
+ <div className="footer-content">
+    <h3> Others information</h3>
+    <p> Facebook:Brahmarupa Secondary School</p>
+    
+ </div>
+
+
+        
+        </footer>
+    
+   
+  );
+};
+
+
+
+
+export default Footer
